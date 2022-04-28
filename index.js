f3c9461fc24064478c74354b3f8e2f9987590504
Travis Bates <tbateswsu@gmail.com>
2022-04-28 12:19:43 -0700

    created functions for add manager, add engineer and add intern

--- index.js
+++ index.js
@@ -1,147 +1,253 @@
 // Team Profile Generator
 
 // REQUIRE
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // CLASS FILES
-const Employee = require('./lib/classes/employee');
-const Manager = require('./lib/classes/manager');
-const Engineer = require('./lib/classes/engineer');
-const Intern = require('./lib/classes/intern');
+const Employee = require("./lib/classes/employee");
+const Manager = require("./lib/classes/manager");
+const Engineer = require("./lib/classes/engineer");
+const Intern = require("./lib/classes/intern");
+
+// HTML TEMPLATE SNIPPETS
+const createMain = require("./src/templates/main");
+const createManager = require("./src/templates/engineer");
+const createEngineer = require("./src/templates/engineer");
+const createIntern = require("./src/templates/intern");
 
 // ROLES
-const employeeRole = {
-    Manager: '',
-    Engineer: '',
-    Intern: ''
-}; 
+// const employeeRole = {
+//     Manager: '',
+//     Engineer: '',
+//     Intern: ''
+// };
 
+// STORE EMPLOYEES HERE FROM PROMP INPUT
+const listOfEmployees = [];
 
+// START OF GENERATE TEAM MEMBERS FUNCTION
+// const createTeamMembers = (data) =>
 
+// first prompt asks for the team manager info
+// second prompt asks whether they want to add an engineer or intern
+// user selects engineer for example, else exit out and generate html page
+// third prompt asks for engineer info
+// fourth prompt asks whether they want to add an intern
+// user selects intern, else exit out and generate the html page
+// fifth prompt asks for intern info
+// generate html page
 
-// Start of generate readme function
-const createTeamMembers = (data) =>
+newEmployee();
 
+const newEmployee = () => {
+  addTeamMemberPrompt().then(function (erole) {
+    switch (erole.role) {
+      case "manager":
+        newManager;
+        break;
+      case "engineer":
+        newEngineer;
+        break;
+      case "intern":
+        newIntern;
+        break;
+      case "finnish":
+        buildTeam;
+      default:
+        console.log("Sorry, no valid choice selected.");
+    }
+  });
+};
 
-// End of generate readme function
+// ADD MANAGER
+const newManager = () => {
+  managerPrompt().then(function (emdata) {
+    let eManager = new Manager(
+      emdata.name,
+      emdata.id,
+      emdata.email,
+      emdata.office
+    );
+    listOfEmployees.push(eManager);
+  });
+  newEmployee();
+};
 
-inquirer
-  .prompt([
+// ADD ENGINEER
+const newEngineer = () => {
+  engineerPrompt().then(function (eedata) {
+    let eEngineer = new Engineer(
+      eedata.name,
+      eedata.id,
+      eedata.email,
+      eedata.githubusername
+    );
+    listOfEmployees.push(eEngineer);
+  });
+  newEmployee();
+};
+
+// ADD INTERN
+const newIntern = () => {
+  internPrompt().then(function (eidata) {
+    let eIntern = new Intern(
+      eidata.name,
+      eidata.id,
+      eidata.email,
+      eidata.school
+    );
+    listOfEmployees.push(eIntern);
+  });
+  newEmployee();
+};
+
+// BUILD MY TEAM
+const buildTeam = () => {};
+
+// MANAGER PROMPT
+const managerPrompt = () => {
+  inquirer.prompt([
     {
-      type: 'input',
-      message: 'Enter team manager name:',
-      name: 'name',
+      type: "input",
+      message: "Enter team manager name:",
+      name: "name",
     },
     {
-        type: 'input',
-        message: 'Employee ID:',
-        name: 'id',
-      },
-      {
-        type: 'input',
-        message: 'Email address:',
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: 'Office number:',
-        name: 'office',
-      },
-    
-      {
-        type: 'list',
-        message: 'Add team member:',
-        name: 'teammember',
-        choices: [
-            'Manager',
-            'Engineer', 
-            'Intern',
-            'Finished'
-        ],
-      }])
-      .then((data) => {
-        console.log(data);
-        const employeeData = createTeamMembers(data);
+      type: "input",
+      message: "Employee ID:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Email address:",
+      name: "email",
+      // could add a email validation step here.
+    },
+    {
+      type: "input",
+      message: "Office number:",
+      name: "office",
+    },
+    {
+      type: "confirm",
+      message: "Confirm Manager role:",
+      name: "manager",
+    },
+    {
+      type: "confirm",
+      message: "Add another team member?",
+      name: "newE",
+    },
+  ]);
+};
 
-inquirer
-  .prompt([
-      {
-        type: 'input',
-        message: 'Enter engineer name:',
-        name: 'name',
-      },
-      {
-        type: 'input',
-        message: 'Employee ID:',
-        name: 'id',
-      },
-      {
-        type: 'input',
-        message: 'Email address:',
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: 'GitHub username:',
-        name: 'githubusername',
-      },
-      {
-        type: 'list',
-        message: 'Add team member:',
-        name: 'teammember',
-        choices: [
-            'Manager',
-            'Engineer', 
-            'Intern',
-            'Finished'
-        ],
-      }])
-      .then((data) => {
-        console.log(data);
-        const employeeData = createTeamMembers(data);
+// ENGINEER PROMPT
+const engineerPrompt = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter engineer name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Employee ID:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "GitHub username:",
+      name: "githubusername",
+    },
+    {
+      type: "confirm",
+      message: "Confirm Engineer role:",
+      name: "engineer",
+    },
+    {
+      type: "confirm",
+      message: "Add another team member?",
+      name: "newE",
+    },
+  ]);
+};
 
-inquirer
-  .prompt([
-      {
-        type: 'input',
-        message: 'Enter intern name:',
-        name: 'name',
-      },
-      {
-        type: 'input',
-        message: 'Employee ID:',
-        name: 'id',
-      },
-      {
-        type: 'input',
-        message: 'Email address:',
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: 'School:',
-        name: 'email',
-      },
-      {
-        type: 'list',
-        message: 'Add team member:',
-        name: 'teammember',
-        choices: [
-            'Manager',
-            'Engineer', 
-            'Intern',
-            'Finished'
-        ],
-      },
+// INTERN PROMPT
+const internPrompt = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter intern name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Employee ID:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Email address:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "School:",
+      name: "email",
+    },
+    {
+      type: "confirm",
+      message: "Confirm Intern role:",
+      name: "intern",
+    },
+    {
+      type: "confirm",
+      message: "Add another team member?",
+      name: "newE",
+    },
+  ]);
+};
 
-  ])
-  .then((data) => {
-    console.log(data);
-    const employeeData = createTeamMembers(data);
+// ADD ANOTHER USER PROMPTS
+const addTeamMemberPrompt = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Select team member role:",
+      name: "role",
+      choices: ["manager", "engineer", "intern"],
+    },
+  ]);
+};
 
-// Write file 
-    fs.writeFile("index.html", employeeData, (err) => 
-            err ? console.error(err) : console.log("Successfully created team member!")
-    );
-  });
+// one for selecting either engineer or intern
+// one for selecting engineer
+// one for selecting intern
+
+//   .then((data) => {
+//     console.log(data);
+//     const employeeData = createTeamMembers(data);
+
+// Write file
+//     fs.writeFile("index.html", employeeData, (err) =>
+//             err ? console.error(err) : console.log("Successfully created team member!")
+//     );
+//   });
+
+//   {
+//     type: 'list',
+//     message: 'Add team member:',
+//     name: 'teammember',
+//     choices: [
+//         'Manager',
+//         'Engineer',
+//         'Intern',
+//         'Finished'
+//     ],
+//     },
